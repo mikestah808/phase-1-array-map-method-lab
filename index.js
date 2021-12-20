ac4996d4877a6b47b925ac6524a468b319a96112
Michael Stafford <michaelstafford808@gmail.com>
2021-12-20 11:37:46 -0600

    commit tree 992c21db5769b2c5775f577666ca0838a0ccfea0

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+// function titleCased(tutorials){
+//     const capitalizeFirstLetter = tutorials.map(sentence => sentence.split(" ")[0].slice(1))
+//     return capitalizeFirstLetter; 
+// }
+
+
+
+// sentence.split(" ")[0].slice(1)
+
 
 const titleCased = () => {
-  return tutorials
+    let tuto = tutorials.map(function toTitleCase(string = " ") {
+        const regex = /^[a-z]{0,1}|\s\w/gi;
+
+        string.match(regex).forEach((char) => {
+            string = string.replace(char, char.toUpperCase());
+        });
+        return string;
+    }
+    
+    )
+
+    return tuto;
 }
